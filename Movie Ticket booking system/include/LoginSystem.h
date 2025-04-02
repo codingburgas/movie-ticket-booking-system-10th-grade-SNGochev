5db720e54ee937b93429f7e8c6157444cfcfcc4b
stilian gochev <SNGochev22@codingburgas.bg>
2025-04-02 14:13:33 +0300

    add login system

--- Movie Ticket booking system/include/LoginSystem.h
+++ Movie Ticket booking system/include/LoginSystem.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef LOGIN_SYSTEM_H
+#define LOGIN_SYSTEM_H
+
+#include <string>
+
+class LoginSystem {
+private:
+    std::string storedUsername;
+    std::string storedPassword;
+    bool isLoggedIn;
+public:
+    LoginSystem();
+    void loginUser();
+    bool isAuthenticated();
+};
+
+#endif
